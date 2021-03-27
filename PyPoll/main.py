9751c42f388bb4aad34a90e2bf09d71a1d675372
M Renee White <unicorn@Ms-MacBook-Pro.local>
2021-03-26 23:22:31 -0600

    update files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,15 +3,20 @@ totalvote = 0
 Canidate = []
 #canidatecount=0
 Votes = {}
+w = 0
+winner = ''
 
 
 #open poling data
 import os
 import csv
-#from collections import Counter
 
+file1 = open('Analysis/PollResults.txt', 'w')
 print("Election Results")
 print("----------------")
+file1.write("Election Results \n")
+file1.write("---------------- \n")
+
 
 votepath=os.path.join ('Resources', 'election_data.csv')
 
@@ -38,15 +43,27 @@ with open(votepath, "r+") as csvfile:
             x += 1
             Votes[Canidate] = x
     print(f"Total Votes: {totalvote}")
-    print("----------------")        
+    file1.write(f"Total Votes: {totalvote} \n")
+    file1.write("---------------- \n")
+    print("----------------") 
         #% of votes each candidate won
     for Person in Votes.keys():
         count = Votes[Person]
         percent = ((count/totalvote)*100)
-
+        
+        
+        file1.writelines((Person)+"   ")
+        file1.writelines(str(count)+"  ")
+        file1.write(str(round(percent,2))+"\n")
         print(Person, count, round(percent,2))
         
-
-print("----------------")
-print("Winner: ")
-print("----------------")
+    
+        if count > w:
+            w = count
+            winner = Person
+            
+    print("----------------")    
+    print ("Winner: " +(winner))
+    file1.write("---------------- \n")
+    file1.writelines ("Winner: " +(winner))
+    file1.close()
